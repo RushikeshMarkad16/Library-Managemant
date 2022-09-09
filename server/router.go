42460495c5d4867e4b6ebaa45b593f6db67356b8
Rushi <rushikeshmarkad0@gmail.com>
2022-09-09 23:39:15 +0530

    Created routes foe user

--- server/router.go
+++ server/router.go
@@ -1,34 +1,38 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
-	_ "github.com/RushikeshMarkad16/Library-Managemant/config"
+	"github.com/RushikeshMarkad16/Library-Managemant/api"
+	"github.com/RushikeshMarkad16/Library-Managemant/config"
+	"github.com/RushikeshMarkad16/Library-Managemant/user"
 	"github.com/gorilla/mux"
 )
 
 const (
-//versionHeader = "Accept"
+	versionHeader = "Accept"
 )
 
 func initRouter(dep dependencies) (router *mux.Router) {
-	// v1:= fmt.Sprintf("application/vnd.%s.v1",config.AppName())
+	v1 := fmt.Sprintf("application/vnd.%s.v1", config.AppName())
 
 	router = mux.NewRouter()
 	router.HandleFunc("/ping", pingHandler).Methods(http.MethodGet)
 
 	//User
 
-	// router.HandleFunc("/users",user.Create(dep.UserService)).Methods(http.MethodPost).Headers(versionHeader,v1)
-	// router.HandleFunc("/users", user.List(dep.UserService)).Methods(http.MethodGet).Headers(versionHeader, v1)
-	// router.HandleFunc("/users/{user_id}", user.FindByID(dep.UserService)).Methods(http.MethodGet).Headers(versionHeader, v1)
-	// router.HandleFunc("/users/{user_id}", user.DeleteByID(dep.UserService)).Methods(http.MethodDelete).Headers(versionHeader, v1)
-	// router.HandleFunc("/users", user.Update(dep.UserService)).Methods(http.MethodPut).Headers(versionHeader, v1)
+	router.HandleFunc("/users", user.Create(dep.UserService)).Methods(http.MethodPost).Headers(versionHeader, v1)
+	router.HandleFunc("/users", user.List(dep.UserService)).Methods(http.MethodGet).Headers(versionHeader, v1)
+	router.HandleFunc("/users/{user_id}", user.FindByID(dep.UserService)).Methods(http.MethodGet).Headers(versionHeader, v1)
+	router.HandleFunc("/users/{user_id}", user.DeleteByID(dep.UserService)).Methods(http.MethodDelete).Headers(versionHeader, v1)
+	router.HandleFunc("/users", user.Update(dep.UserService)).Methods(http.MethodPut).Headers(versionHeader, v1)
 
-	//To DO : swagger and sh implement
+	sh := http.StripPrefix("/docs/", http.FileServer(http.Dir("./swaggerui/")))
+	router.PathPrefix("/docs/").Handler(sh)
 	return
 }
 
 func pingHandler(rw http.ResponseWriter, req *http.Request) {
-	//api.Success(rw, http.StatusOK, api.Response{Message: "pong"})
+	api.Success(rw, http.StatusOK, api.Response{Message: "pong"})
 }
