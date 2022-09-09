7a397f8ee792a290d1fe162ff5e4b2b91bf2313d
Rushi <rushikeshmarkad0@gmail.com>
2022-09-09 23:37:33 +0530

    Updated the db migration config

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"os"
+
 	"github.com/RushikeshMarkad16/Library-Managemant/app"
 	"github.com/RushikeshMarkad16/Library-Managemant/config"
+	"github.com/RushikeshMarkad16/Library-Managemant/db"
 	"github.com/RushikeshMarkad16/Library-Managemant/server"
 	"github.com/urfave/cli"
 )
@@ -24,5 +27,32 @@ func main() {
 				return nil
 			},
 		},
+
+		{
+			Name:  "create_migration",
+			Usage: "create migration file",
+			Action: func(c *cli.Context) error {
+				return db.CreateMigrationFile(c.Args().Get(0))
+			},
+		},
+
+		{
+			Name:  "migrate",
+			Usage: "run db migrations",
+			Action: func(c *cli.Context) error {
+				err := db.RunMigrations()
+				return err
+			},
+		},
+		{
+			Name:  "rollback",
+			Usage: "rollback migrations",
+			Action: func(c *cli.Context) error {
+				return db.RollbackMigrations(c.Args().Get(0))
+			},
+		},
+	}
+	if err := cliApp.Run(os.Args); err != nil {
+		panic(err)
 	}
 }
