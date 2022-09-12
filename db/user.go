022dc6fcb932fc1cf15160bb7b3037023b27f7d1
Rushi <rushikeshmarkad0@gmail.com>
2022-09-12 17:54:41 +0530

    Updated the struct fields

--- db/user.go
+++ db/user.go
@@ -8,12 +8,12 @@ import (
 
 const (
 	createUserQuery = `INSERT INTO user ( 
-    id,first_name,last_name,gender,dob,address,email,password,mob_no,role)
-    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
+    id,first_name,last_name,gender,address,email,password,mob_no,role)
+    VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)`
 
 	listUsersQuery      = `SELECT * FROM user`
-	findUserByIDQuery   = `SELECT * FROM user WHERE id = $1`
-	deleteUserByIDQuery = `DELETE FROM user WHERE id = $1`
+	findUserByIDQuery   = `SELECT * FROM user WHERE id = ?`
+	deleteUserByIDQuery = `DELETE FROM user WHERE id = ?`
 	updateUserQuery     = `UPDATE user SET name = $1, updated_at = $2 where id = $3`
 )
 
@@ -22,16 +22,14 @@ type User struct {
 	First_name string `db:"first_name"`
 	Last_name  string `db:"last_name"`
 	Gender     string `db:"gender"`
-	DOB        string `db:"dob"`
 	Address    string `db:"address"`
 	Email      string `db:"email"`
 	Password   string `db:"password"`
-	Mob_no     int    `db:"mob_no"`
+	Mob_no     string `db:"mob_no"`
 	Role       string `db:"role"`
 }
 
 func (s *store) CreateUser(ctx context.Context, user *User) (err error) {
-	//now := time.Now()
 
 	return Transact(ctx, s.db, &sql.TxOptions{}, func(ctx context.Context) error {
 		_, err = s.db.Exec(
@@ -40,14 +38,11 @@ func (s *store) CreateUser(ctx context.Context, user *User) (err error) {
 			user.First_name,
 			user.Last_name,
 			user.Gender,
-			user.DOB,
 			user.Address,
 			user.Email,
 			user.Password,
 			user.Mob_no,
 			user.Role,
-			// now,
-			// now,
 		)
 		return err
 	})
