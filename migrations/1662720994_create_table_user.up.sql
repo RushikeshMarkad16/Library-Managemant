e2c26efe453701ee5e9ff5081c82cc5f222373e5
Rushi <rushikeshmarkad0@gmail.com>
2022-09-12 18:23:54 +0530

    Updated the mob_no data type

--- migrations/1662720994_create_table_user.up.sql
+++ migrations/1662720994_create_table_user.up.sql
@@ -4,11 +4,10 @@ CREATE TABLE user
   first_name VARCHAR(20) NOT NULL,
   last_name VARCHAR(20) NOT NULL,
   gender VARCHAR(10) NOT NULL,
-  DOB DATE NOT NULL,
   address VARCHAR(50) NOT NULL,
   email VARCHAR(30) NOT NULL,
   password VARCHAR(20) NOT NULL,
-  mob_no INT NOT NULL,
+  mob_no VARCHAR(20) NOT NULL,
   role VARCHAR(10) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE (email)
