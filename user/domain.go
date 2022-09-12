7c8df0a68f8ae1768edacdcb71f36a037219c9d5
Rushi <rushikeshmarkad0@gmail.com>
2022-09-12 18:17:16 +0530

    Updated Validation

--- user/domain.go
+++ user/domain.go
@@ -1,36 +1,28 @@
 package user
 
-import "github.com/RushikeshMarkad16/Library-Managemant/db"
-
-type updateRequest struct {
-	ID         string `json:"id"`
-	First_Name string `json:"name"`
-}
-
-type createRequest struct {
-	First_Name string `json:"first_name"`
+type User struct {
+	ID        string `json:"id"`
+	FirstName string `json:"first_name"`
+	Last_name string `json:"last_name"`
+	Password  string `json:"password"`
+	Gender    string `json:"gender"`
+	Address   string `json:"address"`
+	Email     string `json:"email"`
+	Mob_no    string `json:"mob_no"`
+	Role      string `json:"role"`
 }
 
 type findByIDResponse struct {
-	User db.User `json:"user"`
+	User User `json:"user"`
 }
 
 type listResponse struct {
-	Users []db.User `json:"users"`
+	Users []User `json:"users"`
+	Count int    `json:"total_count"` //to be updated
 }
 
-func (cr createRequest) Validate() (err error) {
-	if cr.First_Name == "" {
-		return errEmptyName
-	}
-	return
-}
-
-func (ur updateRequest) Validate() (err error) {
-	if ur.ID == "" {
-		return errEmptyID
-	}
-	if ur.First_Name == "" {
+func (cr User) Validate() (err error) {
+	if cr.FirstName == "" {
 		return errEmptyName
 	}
 	return
