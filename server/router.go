a6f31b58401de35b62a27d0f5cb608436b58a543
Rushi <rushikeshmarkad0@gmail.com>
2022-09-12 13:20:30 +0530

    Created routers

--- server/router.go
+++ server/router.go
@@ -1,11 +1,9 @@
 package server
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/RushikeshMarkad16/Library-Managemant/api"
-	"github.com/RushikeshMarkad16/Library-Managemant/config"
 	"github.com/RushikeshMarkad16/Library-Managemant/user"
 	"github.com/gorilla/mux"
 )
@@ -15,18 +13,17 @@ const (
 )
 
 func initRouter(dep dependencies) (router *mux.Router) {
-	v1 := fmt.Sprintf("application/vnd.%s.v1", config.AppName())
 
 	router = mux.NewRouter()
 	router.HandleFunc("/ping", pingHandler).Methods(http.MethodGet)
 
 	//User
 
-	router.HandleFunc("/users", user.Create(dep.UserService)).Methods(http.MethodPost).Headers(versionHeader, v1)
-	router.HandleFunc("/users", user.List(dep.UserService)).Methods(http.MethodGet).Headers(versionHeader, v1)
-	router.HandleFunc("/users/{user_id}", user.FindByID(dep.UserService)).Methods(http.MethodGet).Headers(versionHeader, v1)
-	router.HandleFunc("/users/{user_id}", user.DeleteByID(dep.UserService)).Methods(http.MethodDelete).Headers(versionHeader, v1)
-	router.HandleFunc("/users", user.Update(dep.UserService)).Methods(http.MethodPut).Headers(versionHeader, v1)
+	router.HandleFunc("/users", user.Create(dep.UserService)).Methods(http.MethodPost)
+	router.HandleFunc("/users", user.List(dep.UserService)).Methods(http.MethodGet)
+	router.HandleFunc("/users/{user_id}", user.FindByID(dep.UserService)).Methods(http.MethodGet)
+	router.HandleFunc("/users/{user_id}", user.DeleteByID(dep.UserService)).Methods(http.MethodDelete)
+	router.HandleFunc("/users", user.Update(dep.UserService)).Methods(http.MethodPut)
 
 	sh := http.StripPrefix("/docs/", http.FileServer(http.Dir("./swaggerui/")))
 	router.PathPrefix("/docs/").Handler(sh)
