625b39a180e274692c795e5c22c67ae236429457
Rushi <rushikeshmarkad0@gmail.com>
2022-09-12 18:13:44 +0530

    removed unwanted code

--- server/router.go
+++ server/router.go
@@ -8,25 +8,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
-const (
-	versionHeader = "Accept"
-)
-
 func initRouter(dep dependencies) (router *mux.Router) {
 
 	router = mux.NewRouter()
 	router.HandleFunc("/ping", pingHandler).Methods(http.MethodGet)
 
 	//User
-
 	router.HandleFunc("/users", user.Create(dep.UserService)).Methods(http.MethodPost)
 	router.HandleFunc("/users", user.List(dep.UserService)).Methods(http.MethodGet)
-	router.HandleFunc("/users/{user_id}", user.FindByID(dep.UserService)).Methods(http.MethodGet)
-	router.HandleFunc("/users/{user_id}", user.DeleteByID(dep.UserService)).Methods(http.MethodDelete)
+	router.HandleFunc("/users/{id}", user.FindByID(dep.UserService)).Methods(http.MethodGet)
+	router.HandleFunc("/users/{id}", user.DeleteByID(dep.UserService)).Methods(http.MethodDelete)
 	router.HandleFunc("/users", user.Update(dep.UserService)).Methods(http.MethodPut)
 
-	sh := http.StripPrefix("/docs/", http.FileServer(http.Dir("./swaggerui/")))
-	router.PathPrefix("/docs/").Handler(sh)
 	return
 }
 
