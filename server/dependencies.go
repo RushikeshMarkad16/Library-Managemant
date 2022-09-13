97ca7106c7ab506a407e8d7c595bce92df5b7c08
Rushi <rushikeshmarkad0@gmail.com>
2022-09-13 13:42:47 +0530

    Added dependencies for bookservice

--- server/dependencies.go
+++ server/dependencies.go
@@ -2,12 +2,14 @@ package server
 
 import (
 	"github.com/RushikeshMarkad16/Library-Managemant/app"
+	"github.com/RushikeshMarkad16/Library-Managemant/book"
 	"github.com/RushikeshMarkad16/Library-Managemant/db"
 	"github.com/RushikeshMarkad16/Library-Managemant/user"
 )
 
 type dependencies struct {
 	UserService user.Service
+	BookService book.Service
 }
 
 func initDependencies() (dependencies, error) {
@@ -16,8 +18,10 @@ func initDependencies() (dependencies, error) {
 	dbStore := db.NewStorer(appDB)
 
 	userService := user.NewService(dbStore, logger)
+	bookService := book.NewService(dbStore, logger)
 
 	return dependencies{
 		UserService: userService,
+		BookService: bookService,
 	}, nil
 }
