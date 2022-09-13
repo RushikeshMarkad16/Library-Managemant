e387daf64804fa22ceaedf62ef42a3814adfa824
Rushi <rushikeshmarkad0@gmail.com>
2022-09-13 13:44:50 +0530

    Added routes for book operation

--- server/router.go
+++ server/router.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/RushikeshMarkad16/Library-Managemant/api"
+	"github.com/RushikeshMarkad16/Library-Managemant/book"
 	"github.com/RushikeshMarkad16/Library-Managemant/user"
 	"github.com/gorilla/mux"
 )
@@ -19,6 +20,13 @@ func initRouter(dep dependencies) (router *mux.Router) {
 	router.HandleFunc("/users/{id}", user.FindByID(dep.UserService)).Methods(http.MethodGet)
 	router.HandleFunc("/users/{id}", user.DeleteByID(dep.UserService)).Methods(http.MethodDelete)
 	router.HandleFunc("/users", user.Update(dep.UserService)).Methods(http.MethodPut)
+
+	//Book
+	router.HandleFunc("/books", book.Create(dep.BookService)).Methods(http.MethodPost)
+	router.HandleFunc("/books", book.List(dep.BookService)).Methods(http.MethodGet)
+	router.HandleFunc("/books/{id}", book.FindByID(dep.BookService)).Methods(http.MethodGet)
+	router.HandleFunc("/books/{id}", book.DeleteByID(dep.BookService)).Methods(http.MethodDelete)
+	router.HandleFunc("/books", book.Update(dep.BookService)).Methods(http.MethodPut)
 
 	return
 }
