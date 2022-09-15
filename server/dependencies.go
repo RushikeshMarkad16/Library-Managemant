0797e18f07597987a9aba5ac5970c442a9b3dbf3
Rushi <rushikeshmarkad0@gmail.com>
2022-09-15 10:49:12 +0530

    Added Service for the transaction

--- server/dependencies.go
+++ server/dependencies.go
@@ -4,12 +4,14 @@ import (
 	"github.com/RushikeshMarkad16/Library-Managemant/app"
 	"github.com/RushikeshMarkad16/Library-Managemant/book"
 	"github.com/RushikeshMarkad16/Library-Managemant/db"
+	"github.com/RushikeshMarkad16/Library-Managemant/transaction"
 	"github.com/RushikeshMarkad16/Library-Managemant/user"
 )
 
 type dependencies struct {
-	UserService user.Service
-	BookService book.Service
+	UserService        user.Service
+	BookService        book.Service
+	TransactionService transaction.Service
 }
 
 func initDependencies() (dependencies, error) {
@@ -19,9 +21,11 @@ func initDependencies() (dependencies, error) {
 
 	userService := user.NewService(dbStore, logger)
 	bookService := book.NewService(dbStore, logger)
+	transactionService := transaction.NewService(dbStore, logger)
 
 	return dependencies{
-		UserService: userService,
-		BookService: bookService,
+		UserService:        userService,
+		BookService:        bookService,
+		TransactionService: transactionService,
 	}, nil
 }
