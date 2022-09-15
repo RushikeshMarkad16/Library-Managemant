48e05257b7c79104c918e55b99fe5dc9d6b0408d
Rushi <rushikeshmarkad0@gmail.com>
2022-09-15 10:42:12 +0530

    Added the error message if the validation fails

--- book/errors.go
+++ book/errors.go
@@ -3,8 +3,14 @@ package book
 import "errors"
 
 var (
-	errEmptyID   = errors.New("Book ID must be present")
-	errEmptyName = errors.New("Book name must be present")
-	errNoBooks   = errors.New("No book present")
-	errNoBookId  = errors.New("Book is not present")
+	errEmptyID                = errors.New("Book ID must be present")
+	errEmptyName              = errors.New("Book name must be present")
+	errEmptyAuthor            = errors.New("Author name must be present")
+	errZeroCopies             = errors.New("Copies cannot be zero while creation of book")
+	errInvalidPrice           = errors.New("Price should be greater than zero")
+	errInvalidStatus          = errors.New("Invalid Status of book")
+	errInvalidAvailableCopies = errors.New("Available copies cannot be greater than total copies")
+
+	errNoBooks  = errors.New("No book present")
+	errNoBookId = errors.New("Book is not present")
 )
