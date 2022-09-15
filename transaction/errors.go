3e4c2be3e30b5fb1fb59e248dcfaee84f5188036
Rushi <rushikeshmarkad0@gmail.com>
2022-09-15 10:35:41 +0530

    Added the error message if an error occurs in transaction

---
+++ transaction/errors.go
@@ -0,0 +1,10 @@
+package transaction
+
+import "errors"
+
+var (
+	errEmptyBookID       = errors.New("Book ID must be present")
+	errEmptyUserID       = errors.New("User ID must be present")
+	errInvalidReturnDate = errors.New("Return Date cannot be less than issue date")
+	errNoTransactions    = errors.New("No Transactions present")
+)
