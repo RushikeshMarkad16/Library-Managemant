faff780022d4328d3372aecffe11d9e9ffd078f6
Rushi <rushikeshmarkad0@gmail.com>
2022-09-15 10:44:41 +0530

    Updated migration for the transaction table

--- migrations/1663065590_create_table_transaction.up.sql
+++ migrations/1663065590_create_table_transaction.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE Transaction(
     id VARCHAR(64) NOT NULL,
-    issue_date VARCHAR(40) NOT NULL,
-    return_date VARCHAR(40) NOT NULL,
-    actual_return_date VARCHAR(40) NOT NULL,
-    book_id VARCHAR(40) NOT NULL,
-    user_id VARCHAR(40) NOT NULL,
+    issue_date INT NOT NULL,
+    due_date INT NOT NULL,
+    return_date INT,
+    book_id VARCHAR(64) NOT NULL,
+    user_id VARCHAR(64) NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY(user_id) REFERENCES user(id),
     FOREIGN KEY(book_id) REFERENCES book(id)
