342bd91140d9bb4146eb183ea55daf672bf1c7fe
Rushi <rushikeshmarkad0@gmail.com>
2022-09-16 11:42:46 +0530

    Removed unused error msgs

--- transaction/errors.go
+++ transaction/errors.go
@@ -3,8 +3,8 @@ package transaction
 import "errors"
 
 var (
-	errEmptyBookID       = errors.New("Book ID must be present")
-	errEmptyUserID       = errors.New("User ID must be present")
-	errInvalidReturnDate = errors.New("Return Date cannot be less than issue date")
-	errNoTransactions    = errors.New("No Transactions present")
+	errEmptyBookID = errors.New("Book ID must be present")
+	errEmptyUserID = errors.New("User ID must be present")
+	//errInvalidReturnDate = errors.New("Return Date cannot be less than issue date")
+	errNoTransactions = errors.New("No Transactions present")
 )
