39cd4ff47e911cf3eeaeeda8226a584e2f5ccdf4
Rushi <rushikeshmarkad0@gmail.com>
2022-09-18 23:31:14 +0530

    Added the query for the foreign key set zero

--- user/handler.go
+++ user/handler.go
@@ -2,84 +2,34 @@ package user
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/RushikeshMarkad16/Library-Managemant/api"
-	"github.com/golang-jwt/jwt"
 	"github.com/gorilla/mux"
 )
 
-type JWTClaim struct {
-	UserID string `json:user_id`
-	Email  string `json:email`
-	Role   string `json:role`
-	jwt.StandardClaims
+type Authentication struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
-var jwtKey = []byte("bd46*@^g")
-
-func Login() http.HandlerFunc {
+func Login(service Service) http.HandlerFunc {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		var j JWTClaim
+		var j Authentication
 		err := json.NewDecoder(req.Body).Decode(&j)
 		if err != nil {
 			api.Error(rw, http.StatusBadRequest, api.Response{Message: err.Error()})
 			return
 		}
-		jwtString, err := GenerateJWT(j.UserID, j.Email, j.Role)
-		if err != nil {
-			api.Error(rw, http.StatusBadRequest, api.Response{Message: err.Error()})
+		jwtString, err1 := service.GenerateJWT(req.Context(), j.Email, j.Password)
+		if err1 != nil {
+			api.Error(rw, http.StatusBadRequest, api.Response{Message: err1.Error()})
 			return
 		}
 
 		api.Success(rw, http.StatusCreated, jwtString)
 
 	})
-}
-
-func GenerateJWT(UserID string, Email string, Role string) (tokenString string, err error) {
-
-	expirationTime := time.Now().Add(1 * time.Hour)
-	claims := &JWTClaim{
-		Email:  Email,
-		UserID: UserID,
-		Role:   Role,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: expirationTime.Unix(),
-		},
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err = token.SignedString(jwtKey)
-	return
-}
-
-func Authorize(handler http.HandlerFunc, role string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-
-		//TODO:
-		//1. get token from reuqest header
-		//2. decode token
-		//3. check if user exist from token.ID
-		//4. if role is allowed
-		//5. call handler
-
-		tokenString := r.Header.Get("Authorization")
-
-		claims := jwt.MapClaims{}
-		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-			return []byte(jwtKey), nil
-		})
-
-		for key, val := range claims {
-			fmt.Printf("Key: %v, value: %v\n", key, val)
-			fmt.Println(key, val)
-		}
-
-		fmt.Println(token, err)
-
-	}
 }
 
 func Create(service Service) http.HandlerFunc {
@@ -148,6 +98,7 @@ func DeleteByID(service Service) http.HandlerFunc {
 		err := service.deleteByID(req.Context(), vars["id"])
 		if err == errNoUserId {
 			api.Error(rw, http.StatusNotFound, api.Response{Message: err.Error()})
+			//api.Error(rw, http.StatusNotFound, api.Response{Message: "Cannot delete user"})
 		}
 		if err != nil {
 			api.Error(rw, http.StatusInternalServerError, api.Response{Message: err.Error()})
