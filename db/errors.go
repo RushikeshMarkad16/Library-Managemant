473364d58b0a27fe58fdcbfc2bc8690a0634349a
Rushi <rushikeshmarkad0@gmail.com>
2022-09-18 23:34:52 +0530

    Added the new error msgs

--- db/errors.go
+++ db/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrUserNotExist        = errors.New("User does not exist in db")
 	ErrBookNotExist        = errors.New("Book does not exist in db")
 	ErrTransactionNotExist = errors.New("Transaction does not exist in db")
+	ErrUserTakenBook       = errors.New("Cannot delete the user as he has not returned the book")
 )
