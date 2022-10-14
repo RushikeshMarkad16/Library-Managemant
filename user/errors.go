de96e5c6ec02a7c6da1e73024711443e690713fd
Rushi <rushikeshmarkad0@gmail.com>
2022-10-14 11:49:30 +0530

    Added error message

--- user/errors.go
+++ user/errors.go
@@ -4,11 +4,13 @@ import "errors"
 
 var (
 	errEmptyID          = errors.New("User ID must be present")
+	errEmptyFirstName   = errors.New("First name cannot be empty")
+	errEmptyLastName    = errors.New("Last name cannot be empty")
 	errInvalidFirstName = errors.New("Invalid First name")
 	errInvalidLastName  = errors.New("Invalid Last name")
 	errNoUsers          = errors.New("No users present")
 	errNoUserId         = errors.New("User is not present")
-	errWrongPassword    = errors.New("Wrong Password")
+	errWrongPassword    = errors.New("Invalid username/password")
 	errEmptyPassword    = errors.New("Password cannot be empty")
 	errInvalidPassword  = errors.New("Password length must be 6 or more")
 	errInvalidGender    = errors.New("Enter valid gender")
