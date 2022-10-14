007b9b192b8c7122462fbdce8dfe50522f4e8379
Rushi <rushikeshmarkad0@gmail.com>
2022-10-14 11:38:46 +0530

    Updated the datatype of date

--- migrations/1663065590_create_table_transaction.up.sql
+++ migrations/1663065590_create_table_transaction.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Transaction(
     id VARCHAR(64) NOT NULL,
-    issue_date INT NOT NULL,
-    due_date INT NOT NULL,
-    return_date INT,
+    issue_date VARCHAR(100) NOT NULL,
+    due_date VARCHAR(100) NOT NULL,
+    return_date VARCHAR(100),
     book_id VARCHAR(64) NOT NULL,
     user_id VARCHAR(64) NOT NULL,
     PRIMARY KEY(id),
