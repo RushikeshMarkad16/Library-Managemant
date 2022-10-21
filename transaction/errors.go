5fb124ba6ea4e55813764b119dbafae38474fcdb
Rushi <rushikeshmarkad0@gmail.com>
2022-10-21 11:47:04 +0530

    Updated the error message

--- transaction/errors.go
+++ transaction/errors.go
@@ -3,9 +3,8 @@ package transaction
 import "errors"
 
 var (
-	errEmptyBookID = errors.New("Book ID must be present")
-	errEmptyUserID = errors.New("User ID must be present")
-	//errInvalidReturnDate = errors.New("Return Date cannot be less than issue date")
+	errEmptyBookID    = errors.New("Book ID must be present")
+	errEmptyUserID    = errors.New("User ID must be present")
 	errNoTransactions = errors.New("No Transactions present")
 	errAlreadyTaken   = errors.New("You cannot take this book again as it is issued to you already")
 )
