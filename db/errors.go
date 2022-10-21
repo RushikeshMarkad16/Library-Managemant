6e3455942deff33914ff5e4c3dd1b139c96062ef
Rushi <rushikeshmarkad0@gmail.com>
2022-10-21 11:44:48 +0530

    Updated the error message

--- db/errors.go
+++ db/errors.go
@@ -3,10 +3,9 @@ package db
 import "errors"
 
 var (
-	ErrUserNotExist        = errors.New("User does not exist in db")
-	ErrBookNotExist        = errors.New("Book does not exist in db")
-	ErrTransactionNotExist = errors.New("Transaction does not exist in db")
-	ErrIDNotExist          = errors.New("ID does not exist in db")
+	ErrUserNotExist        = errors.New("Invalid user id")
+	ErrBookNotExist        = errors.New("Invalid book id")
+	ErrTransactionNotExist = errors.New("Invalid transaction id")
+	ErrIDNotExist          = errors.New("Invalid user id")
 	ErrAlreadyReturn       = errors.New("Book Already returned")
-	//ErrUserTakenBook       = errors.New("Cannot delete the user as he has not returned the book")
 )
